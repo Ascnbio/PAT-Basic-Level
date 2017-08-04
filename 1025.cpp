86311f4a2ea06d6c5b29b7edfb6a9beae56fb10e
tanzeyy <tan_ze@outlook.com>
2017-08-04 19:49:30 +0800

    添加了1025题

---
+++ 1025.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int main() {
+    int first, k, n, temp;
+    cin >> first >> n >> k;
+    int data[100005], next[100005], list[100005];
+    for (int i = 0; i < n; i++) {
+        cin >> temp;
+        cin >> data[temp] >> next[temp];
+    }
+    int cnt = 0;
+    while (first != -1) {
+        list[cnt++] = first;
+        first = next[first];
+    }
+    for (int i = 0; i < (cnt - cnt % k); i += k)
+        reverse(begin(list) + i, begin(list) + i + k);
+    for (int i = 0; i < cnt - 1; i++)
+        printf("%05d %d %05d\n", list[i], data[list[i]], list[i + 1]);
+    printf("%05d %d -1", list[cnt - 1], data[list[cnt - 1]]);
+    return 0;
+}
