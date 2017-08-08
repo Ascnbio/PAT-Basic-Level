013207fde8b4ee88c7211e3c7126995f19d382ad
tanzeyy <tan_ze@outlook.com>
2017-08-08 11:54:09 +0800

    添加了1036题

---
+++ 1036.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int n;
+    char ch;
+    cin >> n >> ch;
+    int row;
+    if (n % 2 == 0) row = n / 2;
+    else row = n / 2 + 1;
+    for (int i = 0; i < n; i++) cout << ch;
+    cout << endl;
+    for (int i = 0; i < row - 2; i++) {
+        cout << ch;
+        for (int j = 0; j < n - 2; j++) cout << " ";
+        cout << ch << endl;
+    }
+    for (int i = 0; i < n; i++) cout << ch;
+    return 0;
+}
